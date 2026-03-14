bad5ce7e783e4e4d489cf92d29a22f4d300dc60a
Divyansh Sharma <divyanshsharma.cse20@jecrc.ac.in>
2026-03-14 10:27:16 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+      base: '/Coming-soon/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
